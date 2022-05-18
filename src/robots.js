97458aa78389ac9ef45482ee38a465a7b47121bc
vinothjr <103251454+vinothjr@users.noreply.github.com>
2022-05-18 18:37:58 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'vinoth',
+    username: 'Vino',
+    email: 'Vinoth@dec.biz'
+  },
+  {
+    id: 2,
+    name: 'Surendar',
+    username: 'Suray',
+    email: 'Suray@march.tv'
+  },
+  {
+    id: 3,
+    name: 'Kishor',
+    username: 'kishoree',
+    email: 'kishore@april.net'
+  },
+  {
+    id: 4,
+    name: 'Jagan',
+    username: 'Jagann',
+    email: 'Jagann.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'mohann raj',
+    username: 'mohann',
+    email: 'mohan@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'hemnath',
+    username: 'hem',
+    email: 'hemnath@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'praveen',
+    username: 'praveen.Skiles',
+    email: 'Telly.praveen@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Bhooni',
+    username: 'Maxime_bhooni',
+    email: 'Bhooni@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Ruban',
+    username: 'Delphine',
+    email: 'Chaim_ruban@dana.io'
+  },
+  {
+    id: 10,
+    name: 'lokesh',
+    username: 'loki',
+    email: 'lokii.biz'
+  }
+];
